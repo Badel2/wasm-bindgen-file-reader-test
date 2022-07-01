bde798205f23919f2b61c9ed934bc918e677c4b3
Badel2 <2badel2@gmail.com>
2022-07-02 01:24:01 +0200

    Move WebSysFile to separate crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::panic;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_file_reader::WebSysFile;
 
 thread_local! {
     static FILE_READER_SYNC: web_sys::FileReaderSync = web_sys::FileReaderSync::new().expect("failed to create FileReaderSync. is this a web worker context?");
@@ -21,98 +22,6 @@ pub fn init() {
 
     // Logging
     log::debug!("Logger enabled, hello from Rust!");
-}
-
-/// Wrapper around a `web_sys::File` that implements `Read` and `Seek`.
-pub struct WebSysFile {
-    file: web_sys::File,
-    pos: u64,
-}
-
-impl WebSysFile {
-    pub fn new(file: web_sys::File) -> Self {
-        Self { file, pos: 0 }
-    }
-
-    /// File size in bytes.
-    pub fn size(&self) -> u64 {
-        let size_f64 = self.file.size();
-
-        // TODO: error handling in case of overflow here
-        size_f64 as u64
-    }
-}
-
-fn u64_to_f64_safe(x: u64) -> Option<f64> {
-    // TODO: only if x < MAX_SAFE_INTEGER
-    Some(x as f64)
-}
-
-impl Read for WebSysFile {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
-        let buf_len = buf.len();
-        let old_offset = self.pos;
-        let offset_f64 = u64_to_f64_safe(old_offset).expect("offset too large");
-        let offset_end_f64 = u64_to_f64_safe(old_offset.saturating_add(buf_len as u64))
-            .expect("offset + len too large");
-        let blob = self
-            .file
-            .slice_with_f64_and_f64(offset_f64, offset_end_f64)
-            .expect("failed to slice file");
-        let array_buffer = FILE_READER_SYNC.with(|file_reader_sync| {
-            file_reader_sync
-                .read_as_array_buffer(&blob)
-                .expect("failed to read as array buffer")
-        });
-        let array = js_sys::Uint8Array::new(&array_buffer);
-
-        let actual_read_bytes = array.byte_length() as usize;
-        // Copy to output buffer
-        array.copy_to(&mut buf[..actual_read_bytes]);
-        // Update position
-        self.pos = offset_f64 as u64 + actual_read_bytes as u64;
-
-        Ok(actual_read_bytes as usize)
-    }
-}
-
-// Copied these functions from std because they are unstable
-fn overflowing_add_signed(lhs: u64, rhs: i64) -> (u64, bool) {
-    let (res, overflowed) = lhs.overflowing_add(rhs as u64);
-    (res, overflowed ^ (rhs < 0))
-}
-
-fn checked_add_signed(lhs: u64, rhs: i64) -> Option<u64> {
-    let (a, b) = overflowing_add_signed(lhs, rhs);
-    if b {
-        None
-    } else {
-        Some(a)
-    }
-}
-
-impl Seek for WebSysFile {
-    fn seek(&mut self, style: SeekFrom) -> Result<u64, std::io::Error> {
-        // Seek impl copied from std::io::Cursor
-        let (base_pos, offset) = match style {
-            SeekFrom::Start(n) => {
-                self.pos = n;
-                return Ok(n);
-            }
-            SeekFrom::End(n) => (self.size(), n),
-            SeekFrom::Current(n) => (self.pos, n),
-        };
-        match checked_add_signed(base_pos, offset) {
-            Some(n) => {
-                self.pos = n;
-                Ok(self.pos)
-            }
-            None => Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "invalid seek to a negative or overflowing position",
-            )),
-        }
-    }
 }
 
 #[wasm_bindgen]
