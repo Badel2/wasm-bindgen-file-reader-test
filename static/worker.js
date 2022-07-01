f6c5aada149cc4efb55542adc7f5d02cc436ba20
Badel2 <2badel2@gmail.com>
2022-07-02 00:14:41 +0200

    Use async/await syntax in worker.js

--- static/worker.js
+++ static/worker.js
@@ -1,21 +1,15 @@
 importScripts("wasm_bindgen_file_reader.js");
 
-onmessage = function(e) {
+onmessage = async function(e) {
   console.log("Message received from main script", e.data);
-  Rust.wasm_bindgen_file_reader.then(
-    function(wasm_bindgen_file_reader) {
-      /*
-            let workerResult = wasm_bindgen_file_reader.read_at_offset_sync(
-                e.data.file,
-                e.data.offset,
-            );
-            */
-      let workerResult = wasm_bindgen_file_reader.sha256_file_sync(e.data.file);
-      console.log("Posting message back to main script");
-      postMessage(workerResult);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
+  let wasm_bindgen_file_reader = await Rust.wasm_bindgen_file_reader;
+  /*
+    let workerResult = wasm_bindgen_file_reader.read_at_offset_sync(
+        e.data.file,
+        e.data.offset,
+    );
+    */
+  let workerResult = wasm_bindgen_file_reader.sha256_file_sync(e.data.file);
+  console.log("Posting message back to main script");
+  postMessage(workerResult);
 };
